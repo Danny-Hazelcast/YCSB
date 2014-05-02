40d27f1e709bae34f6737c29492efac230359e47
danny <danny@hazelcast.com>
2014-05-02 15:55:12 +0300

    scripting

--- bin/go.sh
+++ bin/go.sh
@@ -7,41 +7,40 @@ if [ "$1" != "" ]; then
     OUTPUT_DIR=../$1
 fi
 
-#rm -rf ${OUTPUT_DIR}
+rm -rf ${OUTPUT_DIR}
 
 for VERSION in ${SYSTEMS[@]}
 do
 
     echo "====== Init Cluster ========"
-#    initCluster ${VERSION} ${CLUSTER_JVMS_PER_BOX} ${CLUSTER_NODES_PER_JVM}
+    initCluster ${VERSION} ${CLUSTER_JVMS_PER_BOX} ${CLUSTER_NODES_PER_JVM}
 
     if [ ${?} == 0 ]; then
 
 
         echo "====== Load phase ========"
-#        loadPhase ${VERSION} ${DB_CLIENTS_PER_BOX} ${INSERTS_PER_DB_CLIENT} ${OPERATIONS_PER_DB_CLIENT} "dbclient.properties" ${WORKLOAD}
+        loadPhase ${VERSION} ${DB_CLIENTS_PER_BOX} ${INSERTS_PER_DB_CLIENT} ${OPERATIONS_PER_DB_CLIENT} "dbclient.properties" ${WORKLOAD}
 
         echo "====== Transaction phase ========"
-#        transactionPhase ${VERSION} ${DB_CLIENTS_PER_BOX} ${INSERTS_PER_DB_CLIENT} ${OPERATIONS_PER_DB_CLIENT} "dbclient.properties" ${WORKLOAD}
+        transactionPhase ${VERSION} ${DB_CLIENTS_PER_BOX} ${INSERTS_PER_DB_CLIENT} ${OPERATIONS_PER_DB_CLIENT} "dbclient.properties" ${WORKLOAD}
 
 
         echo "====== Getting results ========"
-#        downLoadResults ${VERSION} ${DB_CLIENTS_PER_BOX} ${WORKLOAD} ${OUTPUT_DIR}
-#        combineResults ${OUTPUT_DIR} ${VERSION}
+        downLoadResults ${VERSION} ${DB_CLIENTS_PER_BOX} ${WORKLOAD} ${OUTPUT_DIR}
+        combineResults ${OUTPUT_DIR} ${VERSION}
     fi
 
 
     echo "====== Killing phase ========"
-#    killAllJava
+    killAllJava
 done
 
 
 echo "====== Producing Report  ========"
-#saveRunInfo ${VERSION} ${CLUSTER_JVMS_PER_BOX} ${CLUSTER_NODES_PER_JVM} ${DB_CLIENTS_PER_BOX} "dbclient.properties" ${WORKLOAD} ${OUTPUT_DIR}
+saveRunInfo ${VERSION} ${CLUSTER_JVMS_PER_BOX} ${CLUSTER_NODES_PER_JVM} ${DB_CLIENTS_PER_BOX} "dbclient.properties" ${WORKLOAD} ${OUTPUT_DIR}
 
 
 totalInserts=$[${#LOAD_MACHINES[@]}*${DB_CLIENTS_PER_BOX}*${INSERTS_PER_DB_CLIENT}]
 totalOpps=$[${#LOAD_MACHINES[@]}*${DB_CLIENTS_PER_BOX}*${OPERATIONS_PER_DB_CLIENT}]
-
 
 reportResults ${OUTPUT_DIR} ${totalInserts} ${totalOpps}
